dd67c4004bc740466062e84ab1ca496ed0d0e9dd
wafihfzhn <hafizhuddin96@gmail.com>
2024-08-14 16:50:37 +0800

    Remove unused model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_13_135601) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "knowledge_bases", force: :cascade do |t|
-    t.string "question"
-    t.string "thought"
-    t.string "action"
-    t.string "observation"
-    t.string "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
